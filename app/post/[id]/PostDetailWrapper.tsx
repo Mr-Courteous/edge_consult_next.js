a0f2b55a0a4152e9e2fd193a53a4fe18b4bf9f62
Courteous <inumiduncourteous@gmail.com>
2025-08-29 15:39:06 +0100

    edited

--- app/post/[id]/PostDetailWrapper.tsx
+++ app/post/[id]/PostDetailWrapper.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { Post } from '@/types/post'; // Assuming you created this file as per our last conversation
 
 const PostDetailClient = dynamic(() => import('./PostDetailClient'), { ssr: false });
 
 interface PostDetailWrapperProps {
-  post: any; 
+  post: Post | null; 
 }
 
 export default function PostDetailWrapper({ post }: PostDetailWrapperProps) {
